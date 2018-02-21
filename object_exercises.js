f6c8075f637a787a27d471533194d0615769a5be
Alexa Scott <arscott4792@gmail.com>
2018-02-21 12:13:03 -0800

    boss example

--- object_exercises.js
+++ object_exercises.js
@@ -1,52 +1,78 @@
-// // const loaf = {
-// //     "flour": 300,
-// //     "water": 210,
-// //     "hydration": function () {
-// //         return (this.water / this.flour) * 100;
-// //     }
+// // // const loaf = {
+// // //     "flour": 300,
+// // //     "water": 210,
+// // //     "hydration": function () {
+// // //         return (this.water / this.flour) * 100;
+// // //     }
+// // // }
+
+// // // console.log(loaf.flour);
+// // // console.log(loaf.water);
+
+// // // console.log(loaf.hydration());
+// // let prop = {
+// //     foo: 1,
+// //     bar: 2,
+// //     fum: 3,
+// //     quux: 4,
+// //     spam: 5
+// // };
+// // for(let newProp in prop){
+// //     console.log(`${newProp} = ${prop[newProp]}`);
 // // }
 
-// // console.log(loaf.flour);
-// // console.log(loaf.water);
 
-// // console.log(loaf.hydration());
-// let prop = {
-//     foo: 1,
-//     bar: 2,
-//     fum: 3,
-//     quux: 4,
-//     spam: 5
-// };
-// for(let newProp in prop){
-//     console.log(`${newProp} = ${prop[newProp]}`);
+// // const hobbit = {
+// //     'meals': ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'],
+// // }
+
+// // console.log(hobbit.meals[3]);
+
+// let person1 = {
+//     name:'John',
+//     jobTitle:'Marketing'
+// }
+// let person2 = {
+//     name:'Smiley',
+//     jobTitle:'Salesman'
+// }
+// let person3 = {
+//     name:'Bob',
+//     jobTitle:'In-n-out'
+// }
+// let arr = [person1,person2,person3];
+
+// for(let i = 0;i<arr.length;i++){
+//     console.log(arr[i].name,arr[i].jobTitle)
 // }
 
+// // arr.forEach((item,index) => {
+// //     Object.keys())
 
-// const hobbit = {
-//     'meals': ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'],
-// }
 
-// console.log(hobbit.meals[3]);
-
-let person1 = {
+let owner = {
     name:'John',
     jobTitle:'Marketing'
 }
-let person2 = {
+let employee1 = {
     name:'Smiley',
-    jobTitle:'Salesman'
+    jobTitle:'Salesman',
+    boss: 'Juan'
 }
-let person3 = {
+let employee2 = {
     name:'Bob',
-    jobTitle:'In-n-out'
+    jobTitle:'In-n-out',
+    boss: 'Rich'
 }
-let arr = [person1,person2,person3];
+
+let arr = [owner, employee1, employee2];
 
 for(let i = 0;i<arr.length;i++){
-    console.log(arr[i].name,arr[i].jobTitle)
+    if( arr[i].boss !== undefined) {
+    console.log(`${arr[i].name} ${arr[i].jobTitle} reports to ${arr[i].boss}.`);
+} else if( arr[i].boss === undefined) {
+    console.log(`${arr[i].name} ${arr[i].jobTitle} doesn't report to anybody.`);
 }
-// arr.forEach((item,index) => {
-//     Object.keys())
-
+}
 
 
