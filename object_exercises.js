c616c2d6bffe1d5ee82cf0a7569649438b1b1a76
Jeff Vega <jeff@intellibliss.com>
2018-02-21 11:41:53 -0800

    prop passed

--- object_exercises.js
+++ object_exercises.js
@@ -1,12 +1,22 @@
-const loaf = {
-    "flour": 300,
-    "water": 210,
-    "hydration": function () {
-        return (this.water / this.flour) * 100;
-    }
+// const loaf = {
+//     "flour": 300,
+//     "water": 210,
+//     "hydration": function () {
+//         return (this.water / this.flour) * 100;
+//     }
+// }
+
+// console.log(loaf.flour);
+// console.log(loaf.water);
+
+// console.log(loaf.hydration());
+let prop = {
+    foo: 1,
+    bar: 2,
+    fum: 3,
+    quux: 4,
+    spam: 5
+};
+for(let newProp in prop){
+    console.log(`${newProp} = ${prop[newProp]}`);
 }
-
-console.log(loaf.flour);
-console.log(loaf.water);
-
-console.log(loaf.hydration());
